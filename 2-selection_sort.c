7001b1bf1fb184b7eddce01ec6220648be8cc9b8
Nathan Omeri <magembenath@gmail.com>
2022-11-08 18:56:01 -0800

    Selection sort trial

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sort ints using selectionsort
+ * @array: given array
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t temp, i = 0, j;
+	int first;
+
+	while (i < size - 1)
+	{
+		first = array[i];
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < first)
+			{
+				first = array[j];
+				temp = j;
+			}
+		}
+		array[temp] = array[i];
+		array[i] = first;
+		print_array(array, size);
+		i++;
+	}
+}
